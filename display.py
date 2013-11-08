ef5b979e0d938b7ceaec6c0bb07f96cc0c1db088
tommybobbins <tng@chegwin.org>
2013-11-08 22:10:07 +0000

    Performance improvement. Update on demand

--- display.py
+++ display.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 import pygame
 import os
+import subprocess
 from pygame.locals import *
 import gettemperatures
-import mosquitto
+#import mosquitto
 import time,datetime
 import processcalendar
 from gettemperatures import read_temps
 # Every sample_limit loops we need to re-read our parameters
-sample_limit=150
+sample_limit=1500
 sample=0
-fps = 20
+fps = 4 
+target_temp=14
 working_temp_addition=0
-client = mosquitto.Mosquitto("Thermo1")
-client.connect("mqttserver")
+#client = mosquitto.Mosquitto("Thermo1")
+#client.connect("mqttserver")
 ###########################################################
 from evdev import InputDevice, list_devices
 devices = map(InputDevice, list_devices())
@@ -32,14 +34,16 @@ screen = pygame.display.set_mode((320, 240), 0, 32)
 mainloop =  True
 #screen = pygame.display.set_mode((320, 240))
 clock = pygame.time.Clock() 
-button_up = pygame.image.load('icons/OSDChannelUPFO.png')
-button_down = pygame.image.load('icons/OSDChannelDownFO.png')
+button_up_unlit = pygame.image.load('/home/pi/PiThermostat/icons/OSDChannelUpNF.png')
+button_up_lit = pygame.image.load('/home/pi/PiThermostat/icons/OSDChannelUpFO.png')
+button_down_unlit = pygame.image.load('/home/pi/PiThermostat/icons/OSDChannelDownNF.png')
+button_down_lit = pygame.image.load('/home/pi/PiThermostat/icons/OSDChannelDownFO.png')
 myFont = pygame.font.SysFont("arial", 30)
 button_up_x, button_up_y = 20, 60 
-button_down_x, button_down_y = 80, 60 
+button_down_x, button_down_y = 120, 60 
 screen.fill([0,255,0])
-screen.blit(button_up, (button_up_x, button_up_y))
-screen.blit(button_down, (button_down_x, button_down_y))
+screen.blit(button_up_unlit, (button_up_x, button_up_y))
+screen.blit(button_down_unlit, (button_down_x, button_down_y))
 # set up the colors
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
@@ -47,30 +51,62 @@ RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 NAVY_BLUE = ( 0, 0, 192 )
+PURPLE = ( 192, 0, 192 )
 CYAN  = (  0, 255, 255)
 MAGENTA=(255,   0, 255)
 YELLOW =(255, 255,   0)
 
+def screenupdate(time,temp,weather,ideal,tempcolour):
+
+    tempfont = pygame.font.Font(None, 60)
+    tempnow = tempfont.render("%.1f%sC" % (temp, chr(176)) , 1, (tempcolour))
+    timepos = timenow.get_rect(centerx=3*(screen.get_width()/4),centery=13*(screen.get_height()/14))
+    idealpos = idealnow.get_rect(centerx=3*(screen.get_width()/4),centery=1*(screen.get_height()/14))
+    weatherpos = weathernow.get_rect(centerx=1*(screen.get_width()/4),centery=13*(screen.get_height()/14))
+    temppos = tempnow.get_rect(centerx=90,centery=30)
+    timepos = timenow.get_rect(centerx=3*(screen.get_width()/4),centery=13*(screen.get_height()/14))
+    idealpos = idealnow.get_rect(centerx=3*(screen.get_width()/4),centery=1*(screen.get_height()/14))
+    weatherpos = weathernow.get_rect(centerx=1*(screen.get_width()/4),centery=13*(screen.get_height()/14))
+    screen.fill([0,0,0])
+    screen.blit(timenow, timepos)
+    screen.blit(tempnow, temppos)
+    screen.blit(weathernow, weatherpos)
+    screen.blit(idealnow, idealpos)
+    pygame.mouse.set_visible(False)
+    screen.blit(button_up_unlit, (button_up_x, button_up_y))
+    screen.blit(button_down_unlit, (button_down_x, button_down_y))
+    pygame.display.update()
+
+
 while mainloop:
+    from subprocess import call
+    need_to_update=0
     pygame.mouse.set_visible(False)
     tick_time = clock.tick(fps)
     pygame.display.set_caption("Thermostat FPS: %.2f" % (clock.get_fps()))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mainloop = False
-            client.disconnect()
+#            client.disconnect()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mos_x, mos_y = pygame.mouse.get_pos()
 #            print("Pos: %sx%s\n" % pygame.mouse.get_pos())
-            if ((mos_x <= 55) and (mos_x > 1)):
-#                 print "mouse is over 'iconup'"
-                 working_temp_addition += 0.5
-#         	 read_write_temp()
-#	         pygame.event.clear()
-            elif ((mos_x >= 56) and (mos_x <= 140)):
-#                 print "mouse is over 'icondown'"
-                 working_temp_addition -= 0.5
-#                pygame.event.clear()
+            if ((mos_x <= 100) and (mos_x > 1)):
+                  #Mouse if over icon Up
+		  #sudo switch_on_off_backlight.sh on
+		  subprocess.call(["/usr/local/bin/switch_on_off_backlight.sh","on"])
+                  screen.blit(button_up_lit, (button_up_x, button_up_y))
+                  need_to_update=1
+#                  pygame.display.update()
+                  working_temp_addition += 0.5
+            elif ((mos_x >= 101) and (mos_x <= 200)):
+                  #Mouse if over icon Down
+		  #sudo switch_on_off_backlight.sh on
+		  subprocess.call(["/usr/local/bin/switch_on_off_backlight.sh","on"])
+                  screen.blit(button_down_lit, (button_down_x, button_down_y))
+                  need_to_update=1
+#                  pygame.display.update()
+                  working_temp_addition -= 0.5
     if (sample == 0):
 #       print "initialising..."
        (floattemp,target_temp,outside_temp) = read_temps() 
@@ -79,7 +115,7 @@ while mainloop:
 #       print "attempting to run read_temps"
        old_target_temp=target_temp
        (floattemp,target_temp,outside_temp) = read_temps() 
-       client.publish("temperature/sensor", "%f " % floattemp , 1 )
+#       client.publish("temperature/sensor", "%f " % floattemp , 1 )
        if (old_target_temp != target_temp ):
            working_temp_addition=0 
        sample = 1 
@@ -88,38 +124,37 @@ while mainloop:
 #    print sample
     working_temp=int(target_temp) + working_temp_addition
     roundtemp =  (round(floattemp, 1))
-    # Send temp to MQTT
     # Display some text
     font = pygame.font.Font(None, 36)
     now = datetime.datetime.now()
     formatted_time = now.strftime("%d-%h-%Y %H:%M")
     timefont = pygame.font.Font(None, 24)
-    timenow = timefont.render("%s" % (formatted_time) , 1, (WHITE))
-    weathernow = timefont.render("Outside: %i%sC" % (outside_temp, chr(176)) , 1
-, (WHITE))
-    idealnow = timefont.render("Target: %.1f%sC" % (working_temp, chr(176)) , 1, (
-WHITE))
+    timenow = timefont.render("%s" % (formatted_time) , 1, (YELLOW))
+    weathernow = timefont.render("Outside: %i%sC" % (outside_temp, chr(176)) , 1, (WHITE))
+    idealnow = timefont.render("Target: %.1f%sC" % (working_temp, chr(176)) , 1, (WHITE))
     temperature_ratio =  floattemp/working_temp
-    if ( temperature_ratio >= 1 ):
-        fontcolour=RED
+    temperature_ratio =  floattemp/working_temp
+    if ( temperature_ratio >= 1 and temperature_ratio <= 1.025 ):
+        fontcolour=GREEN
     elif ( temperature_ratio <= 1 ):
         fontcolour=BLUE
+    elif ( temperature_ratio >= 1.025 ):
+        fontcolour=RED
     else:
-        fontcolour=GREEN
+        fontcolour=PURPLE
 #    print ("%s\n" % temperature_ratio )
     tempfont = pygame.font.Font(None, 60)
-    temp = tempfont.render("%.1f%sC" % (roundtemp, chr(176)) , 1, (fontcolour))
+    tempnow = tempfont.render("%.1f%sC" % (roundtemp, chr(176)) , 1, (fontcolour))
     timepos = timenow.get_rect(centerx=3*(screen.get_width()/4),centery=13*(screen.get_height()/14))
     idealpos = idealnow.get_rect(centerx=3*(screen.get_width()/4),centery=1*(screen.get_height()/14))
     weatherpos = weathernow.get_rect(centerx=1*(screen.get_width()/4),centery=13*(screen.get_height()/14))
-    temppos = temp.get_rect(centerx=90,centery=30)
-    screen.fill([0,0,0])
-    screen.blit(timenow, timepos)
-    screen.blit(temp, temppos)
-    screen.blit(weathernow, weatherpos)
-    screen.blit(idealnow, idealpos)
-    pygame.mouse.set_visible(False)
-    screen.blit(button_up, (button_up_x, button_up_y))
-    screen.blit(button_down, (button_down_x, button_down_y))
-    pygame.display.update()
+    temppos = tempnow.get_rect(centerx=90,centery=30)
+    if (sample%100 == 0):
+        need_to_update=1
+        screen.blit(button_up_lit, (button_up_x, button_up_y))
+        screen.blit(button_down_lit, (button_down_x, button_down_y))
+    if (need_to_update == 1):
+       print ("screen update")
+       screenupdate(timenow,roundtemp, weathernow, idealnow,fontcolour)
+       need_to_update = 0
 
