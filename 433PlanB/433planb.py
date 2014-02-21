d286a6aecadfa562eda92ce68d6ddb30ae7f7e06
Tim Gibbon <tng@chegwin.org>
2014-02-21 11:18:07 +0000

    Added email check

--- 433PlanB/433planb.py
+++ 433PlanB/433planb.py
@@ -12,39 +12,73 @@ from datetime import datetime
 import redis
 import urllib2
 import re
+import smtplib
+sent_email=0
+from email.mime.text import MIMEText
 threshold_temp = 12
-sys.path.append("/usr/local/lib/python2.7/site-packages/Adafruit/I2C")
 redthis = redis.StrictRedis(host='localhost',port=6379, db=0)
 
 
-def run_boiler():
+def send_email():
+    SERVER = "localhost"
+    FROM = "someone@someemail"
+    TO = ["someoneelse@someotheremail","someoneelse2@someothermeail2"] # must be a list
+    SUBJECT = "Barabbas down!"
+    TEXT = "Barabbas down. I'm driving the boiler."
+    # Prepare actual message
+    message = """\
+    From: %s
+    To: %s
+    Subject: %s
+    
+    %s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    # Send the mail
+    server = smtplib.SMTP(SERVER)
+    server.sendmail(FROM, TO, message)
+    server.quit()
+    return(1)
+
+def run_boiler(sent_email):
     time = datetime.now().time()
     hour,min,sec = str(time).split(":")
     hour = int(hour)
-    if (( hour == 10 ) | (hour == 23)):
+    if (( hour == 10 ) | (hour == 22)):
 #        print ("Firing up boiler")
         response = urllib2.urlopen("http://433board/switchboiler/on/")
-        return ("On")
+#        return ("On")
+        if ( sent_email == 0 ):
+             send_email()
+             return(1)
+        else:
+             return(1)
     else:
-        return ("Done")
+        return (0)
 
 
 while True:
     outside_temp=int(redthis.get("temperature/weather"))
     if (outside_temp > threshold_temp):
 #        print ("Outside temperature %i is greater than %i" %(outside_temp,threshold_temp))
-        exit()
+         print (".")
     elif (outside_temp <= threshold_temp):
 #        print ("Outside temperature %i is less than %i" %(outside_temp,threshold_temp))
 #        print ("See whether we need to run boiler")
         try:
             barabbas_boiler=redthis.ttl("boiler/req")
-            print ("Able to read redis. Queue time is : %i" % barabbas_boiler)
-            print ("-ve means barabbas is down.")
+         #   print ("Able to read redis. Queue time is : %i" % barabbas_boiler)
+         #   print ("-ve means barabbas is down.")
         except:
-            print ("Redis down. We may need to drive the boiler.")
-            run_boiler()
+         #   print ("Bad stuff happening. Priming email")
+         #   print ("Redis down. We may need to drive the boiler.")
+            sent_email=run_boiler(sent_email)
+#            print ("Sent email = %i" % sent_email)
         if (barabbas_boiler < 0):
-            run_boiler()
+            sent_email=run_boiler(sent_email)
+#            print ("Sent email = %i" % sent_email)
+        else:
+#            print ("Barabbas is running the boiler. Priming email")
+            sent_email = 0
+#            print ("Sent email = %i" % sent_email)
     sleep(240)
 
