fde2720764ff44bbe89e4ed09e837ed82a513ee8
Tim Gibbon <tng@chegwin.org>
2014-05-15 14:23:34 +0100

    Another logic missing

--- utilities/velux_auto.py
+++ utilities/velux_auto.py
@@ -19,11 +19,11 @@ while True:
             sleep(14400)
         else:
              sleep(0.1)
-#            print ("Attic temp = %f" % attic_temp)
-#            print ("Half Trigger temp = %f" % velux_half_open_trigger)
-#            print ("Full Trigger temp = %f" % velux_full_open_trigger)
-#            print ("Close Trigger temp = %f" % velux_close_trigger)
-#            print ("Velux state = %s" % velux_state)
+#             print ("Attic temp = %f" % attic_temp)
+#             print ("Half Trigger temp = %f" % velux_half_open_trigger)
+#             print ("Full Trigger temp = %f" % velux_full_open_trigger)
+#             print ("Close Trigger temp = %f" % velux_close_trigger)
+#             print ("Velux state = %s" % velux_state)
 
         if (velux_state == "Half"):
              # We can either open at higher temp or close at Close Trigger Temp
@@ -41,12 +41,13 @@ while True:
              if (attic_temp >= velux_full_open_trigger):
 #                 print ("Nothing to do")
                  job_to_run=0
-             elif (attic_temp >= velux_half_open_trigger):
+             elif (attic_temp >= velux_close_trigger):
                  job_to_run="/usr/local/bin/full_to_half_open.sh"
              elif (attic_temp <= velux_close_trigger):
                  job_to_run="/usr/local/bin/close.sh"
              else:
-                 job_to_run="/usr/local/bin/close.sh"
+                 # attic_temp between velux_c_trig and velux_h_o_trig
+                 job_to_run=0
         elif (velux_state == "Closed"):
              # We can Half open at higher temp
              if (attic_temp >= velux_half_open_trigger):
@@ -57,9 +58,16 @@ while True:
 #                 print ("Nothing to do")
                  job_to_run=0
         else:
-             # Velux state is unknown
-#             print ("Something went wrong")
-             job_to_run=0
+#             print ("Our velux state expired")
+             # We can Half open at higher temp
+             if (attic_temp >= velux_full_open_trigger):
+                 job_to_run="/usr/local/bin/full_open.sh"
+             elif (attic_temp <= velux_half_open_trigger):
+                 job_to_run="/usr/local/bin/close.sh"
+             elif (attic_temp >= velux_half_open_trigger):
+                 job_to_run="/usr/local/bin/half_open.sh"
+             else:
+                 job_to_run="/usr/local/bin/close.sh"
   
     except:
 #        print ("Something went wrong in the try")
@@ -69,8 +77,8 @@ while True:
         if (job_to_run != 0):
 #            print ("At this point we push %s to the queue \n" % job_to_run) 
             redthis.rpush('attic/jobqueue',job_to_run)
-            sleep(240)
-######        redthis.expire("velux/3", 3600 )
+            sleep(120)
+            redthis.expire("velux/3", 3600 ) #Expire in an hour
     except:
         print ("We have no job to run")
-    sleep(240)
+    sleep(120)
