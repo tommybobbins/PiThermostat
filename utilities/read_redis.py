4372c2c1cb272867e918956d50e2962ec9750a92
Tommybobbins <tng@chegwin.org>
2014-05-17 18:45:00 +0100

    Bugfix - change queue runner to try/except

--- utilities/read_redis.py
+++ utilities/read_redis.py
@@ -2,25 +2,28 @@
 import redis
 import subprocess
 from time import sleep
-redthis = redis.StrictRedis(host='433host',port=6379, db=0)
-allowed_jobs = ['/usr/local/bin/bgas', '/usr/local/bin/homeeasy', '/usr/local/bin/drayton', '/usr/local/bin/boot_sequence.sh', '/usr/local/bin/half_open.sh', '/usr/local/bin/close.sh', '/usr/local/bin/full_open.sh' ]
+redthis = redis.StrictRedis(host='433host',port=6379, db=0,socket_timeout=3)
+allowed_jobs = ['/usr/local/bin/bgas', '/usr/local/bin/homeeasy', '/usr/local/bin/drayton']
 #allowed_jobs = ['/usr/local/bin/homeeasy', '/usr/local/bin/drayton']
 
 while True:
-    job_to_run = redthis.lpop('cellar/jobqueue') 
-#    job_to_run = redthis.lindex('jobqueue', 0) 
-    if (job_to_run):
-        job_to_run = job_to_run.split()
-        if job_to_run[0]  in allowed_jobs:
-             try:
-#                print ("Shellscript to run is %s \n" % job_to_run[0]) 
-                subprocess.call(job_to_run) 
-             except:
-#                print ("Unable to find job_to_run")
+    try:
+        job_to_run = redthis.lpop('cellar/jobqueue') 
+#        job_to_run = redthis.lindex('cellar/jobqueue', 0) 
+        if (job_to_run):
+            job_to_run = job_to_run.split()
+            if job_to_run[0]  in allowed_jobs:
+                 try:
+                    print ("Shellscript to run is %s \n" % job_to_run[0]) 
+                    subprocess.call(job_to_run) 
+                 except:
+                    print ("Unable to find job_to_run")
+            else: 
+                print ("Sorry, we are not allowd to run %s \n" % job_to_run[0])
                 continue
-        else: 
-#            print ("Sorry, we are not allowd to run %s \n" % job_to_run[0])
-            continue
-    else:
-#        print ("Idling")
-        sleep(1)
+        else:
+            print ("Idling")
+            sleep(1)
+    except:
+            print ("Unable to read from redis")
+            sleep(10)
