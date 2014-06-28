f3518b5d2875a5621748cbe4ee58b9f501f22f08
Tim Gibbon <tng@chegwin.org>
2014-06-28 17:59:04 +0100

    Added energenie. Increased lock to 2 mins

--- utilities/read_redis.py
+++ utilities/read_redis.py
@@ -6,11 +6,15 @@ redthis = redis.StrictRedis(host='433host',port=6379, db=0,socket_timeout=3)
 allowed_jobs = ['/usr/local/bin/bgas', 
                 '/usr/local/bin/homeeasy',
                 '/usr/local/bin/drayton',
+                '/usr/local/bin/energenie',
                 '/etc/init.d/sensortag.sh',
                 '/usr/local/bin/boot_sequence.sh',
                 '/usr/local/bin/open_to_half_open.sh',
                 '/usr/local/bin/full_open.sh',
                 '/usr/local/bin/closed_to_half_open.sh',
+                '/usr/local/bin/all_open.sh',
+                '/usr/local/bin/all_close.sh',
+                '/usr/local/bin/all_half.sh',
                 '/usr/local/bin/full_close.sh']
 #-rwxr-xr-x 1 pi staff 336 May 14 16:30 boot_sequence.sh
 #-rwxr-xr-x 1 pi staff 249 May 15 16:34 closed_to_half_open.sh
@@ -32,16 +36,16 @@ while True:
         if (job_to_run):
             #print ("We have a job to run")
             job_running = redthis.set('shared/jobqueue', 'True')
-            job_running = redthis.expire('shared/jobqueue', 20) # We don't want to lock the jobqueue for long periods
+            job_running = redthis.expire('shared/jobqueue', 120) # We don't want to lock the jobqueue for long periods
             job_to_run = job_to_run.split() # We only want the binary name, not the arguments
             if job_to_run[0]  in allowed_jobs:
                 # We do have permission
-                # print ("Shellscript to run is %s \n" % job_to_run[0]) 
+                print ("Shellscript to run is %s \n" % job_to_run[0]) 
                 subprocess.call(job_to_run) 
                 sleep(2)
                 job_running = redthis.delete('shared/jobqueue')
             else: 
-       #         print ("Sorry, we are not allowd to run %s \n" % job_to_run[0])
+                print ("Sorry, we are not allowd to run %s \n" % job_to_run[0])
                 job_running = redthis.delete('shared/jobqueue')
                 continue
         else:
