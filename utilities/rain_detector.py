70b728d836585e0c243fda1032b78ef77b069771
Jon Archer <jf_archer@yahoo.com>
2015-09-22 23:40:25 +0100

    added config parser

--- utilities/rain_detector.py
+++ utilities/rain_detector.py
@@ -5,7 +5,18 @@ import redis
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 from time import sleep
-redthis = redis.StrictRedis(host='433host',port=6379, db=0, socket_timeout=3)
+from ConfigParser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('/etc/pithermostat.conf')
+
+redishost=parser.get('redis','broker')
+redisport=parser.get('redis','port')
+redisdb=parser.get('redis','db')
+redistimeout=float(parser.get('redis','timeout'))
+
+redthis=redis.StrictRedis(host=redishost,port=redisport, db=redisdb, socket_timeout=redistimeout)
+
 currState = False
 prevState = False
 
