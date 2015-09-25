6352b6d40270c542b42184048c9ed68451c06ebe
Tim Gibbon <tng@chegwin.org>
2015-09-25 12:53:33 +0100

    Convert redis port to int

--- utilities/redis_sensor.py
+++ utilities/redis_sensor.py
@@ -17,7 +17,7 @@ parser = SafeConfigParser()
 parser.read('/etc/pithermostat.conf')
 
 redishost=parser.get('redis','broker')
-redisport=parser.get('redis','port')
+redisport=int(parser.get('redis','port'))
 redisdb=parser.get('redis','db')
 redistimeout=float(parser.get('redis','timeout'))
 redthis=redis.StrictRedis(host=redishost,port=redisport, db=redisdb, socket_timeout=redistimeout)
