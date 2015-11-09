4bb5f3aa114f6edea561d92d629c0b266c033ef4
Jon Archer <jf_archer@yahoo.com>
2015-11-09 15:11:10 +0000

    Alternative weather parsing script, taking data from a weather underground feed.

---
+++ utilities/wunder.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import redis
+import urllib2
+import json
+from ConfigParser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('/etc/pithermostat.conf')
+
+redishost=parser.get('redis','broker')
+redisport=int(parser.get('redis','port'))
+redisdb=parser.get('redis','db')
+redistimeout=float(parser.get('redis','timeout'))
+redthis=redis.StrictRedis(host=redishost,port=redisport, db=redisdb, socket_timeout=redistimeout)
+
+loc=parser.get('wunder', 'location')
+apikey=parser.get('wunder', 'apikey')
+
+url='http://api.wunderground.com/api/' + apikey + '/geolookup/conditions/q/pws:' + loc + '.json'
+f = urllib2.urlopen(url)
+json_string = f.read()
+parsed_json = json.loads(json_string)
+temp_c = parsed_json['current_observation']['temp_c']
+redthis.set("temperature/weather",temp_c)
+
+f.close()
