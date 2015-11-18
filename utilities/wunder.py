5fa41d0e8d9e3f45bac771811c42c1d7e5921232
jfarcher <jf_archer@yahoo.com>
2015-11-18 16:42:03 +0000

    added values for other weather elements and publish them to keys in redis. To be used for a weather display screen

--- utilities/wunder.py
+++ utilities/wunder.py
@@ -18,10 +18,17 @@ loc=parser.get('wunder', 'location')
 apikey=parser.get('wunder', 'apikey')
 
 url='http://api.wunderground.com/api/' + apikey + '/geolookup/conditions/q/pws:' + loc + '.json'
+print url
 f = urllib2.urlopen(url)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 temp_c = parsed_json['current_observation']['temp_c']
+weather = parsed_json['current_observation']['weather']
+wind_mph = parsed_json['current_observation']['wind_mph']
+wind_dir = parsed_json['current_observation']['wind_dir']
 redthis.set("temperature/weather",temp_c)
+redthis.set("weather/weather", weather)
+redthis.set("weather/wind_mph", wind_mph)
+redthis.set("weather/wind_dir", wind_dir)
 
 f.close()
