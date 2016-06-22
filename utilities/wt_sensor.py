2f9175032e32161bf774a49fbb38ff299e4c7aec
Tim Gibbon <tng@chegwin.org>
2016-06-22 22:29:20 +0100

    Corrected battery voltage bug with wireless things

--- utilities/wt_sensor.py
+++ utilities/wt_sensor.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python
-######
-#Script to read Wireless things Temperature sensor and report into 
-#redis via Django interface
-#Requires python-pyserial and pycurl
-#Incoming strings look like
+import serial,re
+from time import sleep, gmtime, strftime
+import pycurl
+host_url = "http://433host:5537/checkinwt/"
+import logging
+from time import sleep
+FORMAT = "%(asctime)s:%(message)s"
+logging.basicConfig(filename='/home/pi/wireless_things.txt',level=logging.INFO,format=FORMAT)
+#AATEMP029.5
 #Wed, 15 Jun 2016 12:43:32: AATEMP020.5
 #Wed, 15 Jun 2016 12:43:32: AAAWAKE----
 #Wed, 15 Jun 2016 12:43:32: AABATT3.07-
 #Wed, 15 Jun 2016 12:43:32: AASLEEPING-
-#Curl request looks like
-#http://433host/checkinwt/AA/temperature/18.9/ 
-######
-import serial,re
-from time import sleep, gmtime, strftime
-import pycurl
-host_url = "http://433host/checkinwt/"
 regex_temp = re.compile(r'(\w+)TEMP(.*)')
 regex_batt = re.compile(r'(\w+)BATT(.*)')
 non_decimal = re.compile(r'[^\d.]+')
@@ -32,23 +29,22 @@ def post_to_433(host,node,torv,value):
     req_url.write(torv)
     req_url.write(value)
     req_url.write('/')
-#    print ("Requesting %s \n" % req_url.getvalue())
+    logging.info("Requesting %s \n" % req_url.getvalue())
     c.setopt(c.URL, req_url.getvalue())
     c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
     body = buffer.getvalue()
-#    print(body)
+    logging.info(body)
 
 #print (strftime("%a, %d %b %Y %H:%M:%S: Starting\n", gmtime()))
 
 ser = serial.Serial(DEVICE, BAUD)
 while True:
-#    print("%s: Checking..." % strftime("%a, %d %b %Y %H:%M:%S", gmtime()))
+#    logging.info("Checking...")
     n = ser.inWaiting()
     if n != 0:
         msg = ser.read(n)
-#        print("%s: %s" % (strftime("%a, %d %b %Y %H:%M:%S", gmtime()), msg))
         temps = msg.split('a')
         for temp in temps:
             tempmatch = regex_temp.search(temp)
@@ -56,21 +52,20 @@ while True:
                 try:
                     id = tempmatch.group(1)
                     idtemp = float(tempmatch.group(2))
-#                    print ("%s returns %f degrees\n" % (id,idtemp))
+                    logging.info ("%s returns %f degrees\n" % (id,idtemp))
                     post_to_433 (host_url, id, "/temperature/", idtemp )
                 except:
-                    print("%s: Unable to parse %s" % (strftime("%a, %d %b %Y %H:%M:%S", gmtime()), temp))
+                    logging.info("Unable to parse %s" % msg)
             battmatch = regex_batt.search(temp)
             if battmatch:
                 try:
                     id = battmatch.group(1)
                     idvolt = (battmatch.group(2))
                     decimal_volts = re.sub('[^\d\.]+', '', idvolt)
+                    #logging.info ("battery returns %s Volts\n" % decimal_volts)
                     decimal_volts = float(decimal_volts)
-                    post_to_433 (host_url, "/voltage/", decimal_volts )
-#                    print ("%s battery returns %s Volts\n" % (id,decimal_volts))
+                    post_to_433 (host_url, id, "/voltage/", decimal_volts )
                 except:
-                    print("%s: Unable to parse %s" % (strftime("%a, %d %b %Y %H:%M:%S", gmtime()), temp))
-                    print("%s: Unable to parse %s" % (strftime("%a, %d %b %Y %H:%M:%S", gmtime()), msg))
-#                post_to_433 (host_url, id, "/voltage/", decimal_volts )
+                    logging.info("Unable to parse %s" % msg)
+                    logging.info ("%s battery returns %s Volts\n" % (id,decimal_volts))
     sleep(1 * 60)
