54c66c42353ddb2c5b6b884fe93aae3b2af81416
Tim Gibbon <tng@chegwin.org>
2021-03-27 09:42:16 +0000

    Split Makefile up

--- bin/unit_test.py
+++ bin/unit_test.py
@@ -18,38 +18,27 @@ class TestSum(unittest.TestCase):
     def test_homepage(self):
         r = http.request('GET', "http://hotf/")
         self.assertEqual(r.status, 200, "Should be 200") 
-        print ("hp test done")
 
     def test_cal(self):
         r = http.request('GET', "http://hotf/feed/calendar/upcoming/1/")
         self.assertEqual(r.status, 200, "Should be 200")
-        print ("cal test done")
 
-    def waterredisvalue(self):
-        print ("In water")
-        water_value=self.redthis.get("water/req").decode('utf-8')
-        self.assertEqual(water_value,r'^on|off$' , "on or off")
-#        print ("Water redis value done")
+    def test_redis_water_value(self):
+        water_value=redthis.get("water/req").decode('utf-8')
+        self.assertRegex(water_value, "on" , "on or off")
 
-    def test_redis_ttl(self):
-        boiler_time=redthis.ttl("boiler/req")
-        self.assertLessEqual(boiler_time, 300, "Boiler time less than 300")
-        print ("redis ttl done")
+    def test_redis_water_ttl(self):
+        water_time=redthis.ttl("water/req")
+        self.assertLessEqual(water_time, 300, "Boiler time less than 300")
 
-#    def water_redis_value2(self):
-#        print ("In water")
-#        self.assertRegex("water","water", "on or off")
-
-    def test_redis_value(self):
+    def test_redis_boiler_value(self):
         boiler_value=redthis.get("boiler/req").decode('utf-8')
         self.assertRegex(boiler_value,r'^On|Off$', "On or Off")
-        print ("redis value done")
 
-#    def water_redis_value2(self):
-#        print ("In water")
-#        self.assertRegex("bobbins","bobbins", "on or off")
-#        water_value=redthis.get("water/req").decode('utf-8')
-#        print ("Water redis value done")
+    def test_redis_boiler_ttl(self):
+        boiler_time=redthis.ttl("boiler/req")
+        self.assertLessEqual(boiler_time, 300, "Boiler time less than 300")
+
 
 if __name__ == '__main__':
     unittest.main()
